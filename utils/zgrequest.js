f2b3a7913b907e4bc3c206ec1e3d0461f1cdf2b4
WTTE <2973682306@qq.com>
2020-12-22 21:42:23 +0800

    极客商城小程序上传

---
+++ utils/zgrequest.js
@@ -0,0 +1,45 @@
+const baseUrl = "http://42.193.168.201:8082"
+
+
+// /api/getlunbo  --->  此时 proxy生效了,也就是说在h5中请求路径不需要加baseUrl
+// baseUrl + url  --->  http://42.193.168.201:8082/api/getlunbo  此时配置的代理没有生效
+
+export function myRequestGet(url, data) {
+	var requesturl = baseUrl+url;
+	// #ifdef H5
+	var requesturl = url;
+	// #endif
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: requesturl,
+			method: "GET",
+			data: data,
+			success: function(res) {
+				resolve(res.data)
+			},
+			fail: function(err) {
+				reject(err)
+			}
+		})
+	})
+}
+
+
+export function myRequestPost(url, data) {
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: baseUrl + url,
+			header: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			method: "POST",
+			data: data,
+			success: function(res) {
+				resolve(res.data)
+			},
+			fail: function(err) {
+				reject(err)
+			}
+		})
+	})
+}
